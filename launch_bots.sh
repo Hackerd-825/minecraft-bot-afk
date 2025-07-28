bcafc99eb9b5216f7f92b436363d237e41310cd8
hackerd_825 <155019837+Hackerd-825@users.noreply.github.com>
2025-07-28 09:40:30 +0200

    Delete launch_bots.sh

--- launch_bots.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Lance 3 bots AFK Minecraft en arrière-plan
-
-python3 bot_afk.py BotAFK1 play.example.com 25565 &
-python3 bot_afk.py BotAFK2 play.example.com 25565 &
-python3 bot_afk.py BotAFK3 play.example.com 25565 &
-
-wait
