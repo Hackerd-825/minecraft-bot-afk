e8f0ffee1b579067b8ee690a596626ee4faf06ff
hackerd_825 <155019837+Hackerd-825@users.noreply.github.com>
2025-07-28 02:24:33 +0200

    Add files via upload

---
+++ launch_bots.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Lance 3 bots AFK Minecraft
+
+start cmd /k python bot_afk.py BotAFK1 play.example.com 25565
+start cmd /k python bot_afk.py BotAFK2 play.example.com 25565
+start cmd /k python bot_afk.py BotAFK3 play.example.com 25565
